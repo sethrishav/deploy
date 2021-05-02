223f2247ace8f41b6ed64668e3d62a11ef53bae8
rishu <sethrishav>
2021-05-02 17:02:46 +0530

    add

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './popup.css';
 
 const Popup = (props) => {
-  return(
-    <div className="popup--back">
-      <div className="popup--content">
-          <div className="popup--close" onClick={props.closePopup}>x</div>
-      </div>
-    </div>
-  )
+    return (
+
+        <
+        div className = "popup--back" >
+        <
+        div className = "popup--content" >
+        <
+        h1 align = "center" > popup < /h1> <
+        div className = "popup--close"
+        onClick = { props.closePopup } > x < /div> <
+        /div> <
+        /div>
+    )
 }
 
 
